6a8e2ea11cf9a7342beffb73b7eddbd4acce83fb
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-07 09:50:58 -0500

    css updated

--- src/components/ApplyPage/ApplyPage.jsx
+++ src/components/ApplyPage/ApplyPage.jsx
@@ -1,32 +1,35 @@
 import React from 'react';
 import TextareaAutosize from '@mui/material/TextareaAutosize';
+import '../ApplyPage/ApplyPage.css'
 
 function Application() {
 
     return (
-        <div>
+        <div className="application">
             <h4>Apply Here:</h4>
             <form>
-                <input type="text" placeholder="Full Name"/>
-                <input type="email" placeholder="Email"/>
-                <input type="phone" placeholder="Phone"/>
-                <input type="street-address" placeholder="Street Address"/>
-                <input type="city, state, zip" placeholder="City, State, Zip"/>
+                <input className="input" type="text" placeholder="Full Name"/>
+                <input className="input" type="email" placeholder="Email"/>
+                <input className="input" type="phone" placeholder="Phone"/>
+                <input className="input" type="street-address" placeholder="Street Address"/>
+                <input className="input" type="city, state, zip" placeholder="City, State, Zip"/>
                 <TextareaAutosize
+                    className="input text-area"
                     maxRows={8}
                     aria-label="maximum height"
                     placeholder="Tell us about yourself."
                     style={{ width: 200 }}
                 />
                 <TextareaAutosize
+                    className="input text-area"
                     maxRows={8}
                     aria-label="maximum height"
                     placeholder="Why are you the best candidate?"
                     style={{ width: 200 }}
                 />
-                <input type="file"/>
-                <input type="url" placeholder="Video URL"/>
-                <input type="submit"/>
+                <input className="input" type="file"/>
+                <input className="input" type="url" placeholder="Video URL"/>
+                <input className="submit-btn" type="submit"/>
             </form>
         </div>
     );
