ca2b1b4f9ea76056176f367475e8e425ec8de88f
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-08 15:24:47 -0500

    admin detail is displaying application info

--- server/routes/admin.router.js
+++ server/routes/admin.router.js
@@ -84,4 +84,38 @@ adminRouter.put('/:applicationID', (req, res) => {
   })
 });
 
+adminRouter.get('/:id', (req, res) => {
+  // GET route code here
+  const sqlQuery = `
+    SELECT
+        application.id,
+        application.status,
+        application.name,
+        application.email,
+        application.phone,
+        application.address,
+        application.address2,
+        application.about,
+        application."whyYou",
+        application.file,
+        application.video,
+        comment.comment,
+        vote.vote
+    FROM application
+    LEFT JOIN comment
+        on application.id = comment.application_id
+    LEFT JOIN vote
+        on application.id = vote.application_id
+    WHERE application.id = $1;`
+    pool.query(sqlQuery, [req.params.id])
+        .then((results) => {
+            // console.log("router side >>>>>>>>>>", results);
+            res.send(results.rows[0])
+        })
+        .catch((err) => {
+            console.log('GET failed in admin router', err);
+        });
+});
+
+
 module.exports = adminRouter;
--- src/components/AdminDetailView/AdminDetailView.jsx
+++ src/components/AdminDetailView/AdminDetailView.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useSelector , useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+import './AdminDetailView.css'
 
 function adminDetailView() {
     const dispatch = useDispatch();
@@ -17,9 +18,21 @@ function adminDetailView() {
     }, [params.id]);
 
     return (
-        <div>
+        <div className="float-Detail">
             <h3>Application</h3>
-            <h4>{application.name}</h4>
+            <div className="float-infoLeft">
+                <h4>Name: {application.name}</h4>
+                <h4>Email: {application.email}</h4>
+                <h4>Phone: {application.phone}</h4>
+                <h4>Address: {application.address}{application.address2}</h4>
+                <h4>Email: {application.email}</h4>
+            </div>
+            <div className="float-infoRight">
+                <h4>About Applicant: {application.about}</h4>
+                <h4>Why are you the best applicant? {application.whyYou}</h4>
+                <h4>File Uploaded: {application.file}</h4>
+                <h4>Applicant Video: {application.video}</h4>
+            </div>
         </div>
     )
 }
