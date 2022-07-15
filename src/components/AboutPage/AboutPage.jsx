3ab140b97f9df8b290a86acb304566602fd918df
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-15 14:22:15 -0500

    some styling updated

--- src/components/AboutPage/AboutPage.jsx
+++ src/components/AboutPage/AboutPage.jsx
@@ -3,13 +3,13 @@ import './AboutPage.css'
 
 function AboutPage() {
   return (
-    <div className="container">
+    <div className="about-container">
       <div className="info">
         <p>"Jerry Snyder, the Iron Range band leader everyone liked..." This caption from a Star Tribune article dated July 24, 2020 captured our band director well. Jerry Snyder made people in his bands feel welcomed, important, happy, and capable. His students over 30 years received a quality music education under his skillful direction. Many of his students have carried music into their adult lives, whether professionally or just for fun. To honor the man who gifted so many with mentoring, friendship, and quality music experiences in the Chisholm Schools and various community and church groups in Minnesota and Arizona, we have established a fund in his honor.</p>
         <p>For many of us, “band with Mr. Snyder” tops our list. We received a quality music education under his skillful direction. Do you remember how he made us feel to be in his band--welcomed, important, skilled, and happy. The joy we experienced from our CHS band days launched many of us into an adult life which has included music at some time, whether professionally or just for fun. Now we pass on that love of music to people who matter to us. If you are not a CHS alum, but you had the privilege of making music with Jerry Snyder in another place and time, you are likely share these same feelings. Jerry gifted so many with mentoring, friendship, and quality music experiences. He did this through community and church groups in Minnesota and Arizona, as well as in the Chisholm Schools. As you may know, Jerry Snyder passed away July 11, 2020 at age 81 from COVID-related complications. To honor him, we established a scholarship in his name. Will you join us in honoring him by donating to the Jerry Snyder Memorial Fund?</p>
         <p>The mission of JSMF is to honor the musical legacy built by Jerry Snyder at Chisholm High School and to carry it forward by raising funds and awarding at least one scholarship per year to a deserving student from Chisholm High School.</p>
         <p>Thanks to generous donors in our inaugural year, we were excited to award our first $2,500 scholarship in May of 2021 to Kenzie Holewa! Our initial goal was to raise at least $10,000, enough to fund multiple scholarships in upcoming years. If you gave last year, would you consider making another donation this year? Or would you make a first-time donation?</p>
-        <button><a href="https://gf.me/u/zb8by7" target="_blank">Donate</a></button>
+        <button className="donate"><a href="https://gf.me/u/zb8by7" target="_blank">Donate</a></button>
       </div>
     </div>
   );
