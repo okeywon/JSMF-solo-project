b855b471c10dd68870063502907839341367a5e0
Courtney Azar <courtney.a.azar@gmail.com>
2022-07-17 18:44:19 -0500

    styling updated with QR code and card inputs.

--- src/components/DonationPage/DonationPage.jsx
+++ src/components/DonationPage/DonationPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import {CardNumberElement} from '@stripe/react-stripe-js';
+import { CardExpiryElement } from '@stripe/react-stripe-js';
 import './DonationPage.css';
 
 function DonatePage() {
@@ -10,14 +11,22 @@ function DonatePage() {
 
     return (
         <div className="donate-container">
-            <div>
+            <div className="contentLeft">
                 <p>Care to contribute to the Jerry Snyder Memorial Fund?</p>
-                <p>Proceeds are tax deductible! Please request a Tax Credit through the email here: jerrysnyderscholarship@gmail.com</p>
-                <button className="donate-btn-page"><a href="https://gf.me/u/zb8by7" target="_blank">Donate</a></button>
+                <p>Proceeds are tax deductible! Please request a Tax Credit via email: jerrysnyderscholarship@gmail.com</p>
+                <div className="gfmBtn">
+                    <button className="donate-btn-page"><a href="https://gf.me/u/zb8by7" target="_blank">Donate via GoFundMe</a></button>
+                </div>
                 <p>How much would you like to donate today?</p>
-                <input type="decimal" placeholder="$10.00"/>
-                <CardNumberElement />
-                <button onClick={()=>{handleSubmit}}>Submit</button>
+                <div className="cardInput">
+                    <input type="decimal" placeholder="$10.00"/>
+                    <input id="ccn" type="tel" inputmode="numeric" pattern="[0-9\s]{13,19}" autocomplete="cc-number" maxlength="19" placeholder="1234 1234 1234 1234"/>
+                    <input type="text" placeholder="MM / YY"/>
+                    <button className="submit" onClick={()=>{handleSubmit}}>Submit</button>
+                </div>
+            </div>
+            <div className="contentRight">
+                <img src="./images/qr_test_7sI8AD444fxd6xafYY.png"/>
             </div>
         </div>
   );
